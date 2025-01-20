f106aff9ba14950d9db4a7da8bd92cd2cf6ffd38
Kalin ILIEVSKI <ilievski.kalin@gmail.com>
2025-01-20 01:13:12 +0100

    get_total_spent implementacija

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from extensions import db
 from models import User
 from models import UserSpending
+from sqlalchemy import func
 
 
 app = Flask(__name__)
@@ -23,10 +24,12 @@ def get_users():
     users = User.query.all()
     return jsonify([user.to_dict() for user in users])
 
-
-
-@app.route('/total/<int:user_id> ', methods=['GET'])
+@app.route('/total/<int:user_id>', methods=['GET'])
 def get_total_spent(user_id):
-    userSpending = UserSpending.query.get_or_404(user_id)
-    return jsonify(userSpending.to_dict())
+    total_spent = db.session.query(func.sum(UserSpending.money_spent)).filter_by(user_id=user_id).scalar()
 
+    if total_spent is None:
+        return jsonify({"error": f"No spending records found for user_id {user_id}"}), 404
+
+    total_spent = round(total_spent, 2)
+    return jsonify({"user_id": user_id, "total_spent": total_spent})
--- models.py
+++ models.py
@@ -17,25 +17,15 @@ class User(db.Model):
 
 class UserSpending(db.Model):
     __tablename__ = 'user_spending'
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     money_spent = db.Column(db.Float, nullable=False)
     year = db.Column(db.Integer, nullable=False)
 
     def to_dict(self):
-        return {'spent': self.money_spent,}
+        return {
+            'id': self.user_id,
+            'spent': self.money_spent,
+
+                }
 
 
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     age = db.Column(db.Integer, nullable=False)
-#
-#     def to_dict(self):
-#         return {
-#             'id': self.id,
-#             'name': self.name,
-#             'email': self.email,
-#             'age': self.age,
-#         }
